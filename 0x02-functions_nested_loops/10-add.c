f8941a806aad1c98f59efec627f0430840c139f4
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-03-16 16:40:52 -0700

    this is the start of the commits iiin the filder

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/** 
+ * main - Prints the 9 times 0table, starting from 0
+ *
+ * Return: (0) Always
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the readme for the foldder for functions and nested loops
