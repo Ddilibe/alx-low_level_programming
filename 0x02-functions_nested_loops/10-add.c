8c2e1b2c3d172981963988a3f834065eb4fc102d
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-03-16 16:57:58 -0700

    We ar bout up

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,12 +2,12 @@
 /**
  * add - Adds two integer and retuns
  * the result
- * 
+ *
  * @a: number one.
  * @b: number two.
  *
  * Return: Add of number one and number two.
- */ 
+ */
 
 
 int add(int a, int b)
