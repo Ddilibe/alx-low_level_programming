8cd3d0590dd2b9a9c454b1e3a012999c57e26c2b
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-03-16 16:52:17 -0700

    Updated the 10-adds file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,13 @@
 #include "main.h"
-/** 
+/**
  * main - Prints the 9 times 0table, starting from 0
+ * 
+ * @a: number one.
+ * @b: number two.
  *
  * Return: (0) Always
- */
+ */ 
+
 
 int add(int a, int b)
 {
