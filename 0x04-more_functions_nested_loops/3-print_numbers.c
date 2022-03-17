d085c2761614fb9d168f191a9cce7fc7f3279ce4
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-03-17 16:59:22 -0700

    Finished the function that prints the numbers from 0 to 9 then followed ny a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints all the numbers from 0 to 9
+ * Return: No return.
+ */
+
+void print_numbers(void)
+{
+	int j;
+
+	for (j = 48; j <= 57; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
