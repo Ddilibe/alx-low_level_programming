5a256311708c2300bb2794abc526e0766bded7ce
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-03-17 17:15:35 -0700

    I beleive in numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints all the numbers from 0 to 9 with  the exceptionn of 2 and 4
+ * Return: No return.
+ */
+
+void print_most_numbers(void)
+{
+	int j;
+
+	for (j = 48; j <= 57; j++)
+	{
+		if ((j == 50) || (j == 52))
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(j);
+		}
+	}
+	_putchar('\n');
+}
