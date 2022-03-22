d01c657cf7eaf8ae7ad0dd8945135a12878754e0
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-03-22 06:01:39 +0100

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,7 +3,7 @@
 
 /**
  * _strcpy - copies the string pointed to by src
- * including the treminating null byte, to the 
+ * including the treminating null byte, to the
  * buffer pointed to by dest.
  * @dest: destination
  * @src: source
