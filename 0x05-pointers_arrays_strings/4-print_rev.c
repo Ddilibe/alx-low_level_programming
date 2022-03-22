82ad50c57745a884c9cc7bf493a7c41a4ed20df2
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-03-22 04:00:05 +0100

    This is amazing

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_rev - Prints a string in reverse
+ * @s: string to be inputed
+ * Return: no return
+ */
+
+void print_rev(char *s)
+{
+	int i, len;
+
+	len = strlen(s);
+	for (i = len; i >= 0; i--)
+	{
+		printf("%c",s[i]);
+	}
+}
