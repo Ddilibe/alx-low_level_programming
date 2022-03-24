d76c8f19f6c2a5ee9c2d061803aee8084fabf20e
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-03-24 12:52:30 +0100

    Yyess this is what i want

--- str.c
+++ str.c
@@ -1,6 +1,6 @@
 #include "stdio.h"
 
-void swap(char *dest, char *src);
+char swap(char *dest, char *src);
 int main(void)
 {
 	char *fot = "This";
@@ -13,18 +13,28 @@ int main(void)
 	printf("fot : %s\ngit : %s\n", fot, git);
 	return (0);
 }
-void swap(char *dest, char *src){
-	int count = 0, count2 = 0;
+char swap(char *dest, char *src){
+//	int count = 0, count2 = 0;
+//
+//	while (*(dest + count) != '\0')
+//		count++;
+//
+//	while (count++ >= 0)
+//	{
+//		*(dest + count) = *(src + count2);
+//		if (*(src + count2) == '\0')
+//			break;
+//		count++;
+//		count2++;
+//	}
+	int a =-1, i;
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	do {
+		a++;
+		dest[i] = src[a];
+		i++;
+	} while (src[a] != '\0');
 
-	while (*(dest + count) != '\0')
-		count++;
-
-	while (count++ >= 0)
-	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count2++;
-	}
+	return (dest);
 }
