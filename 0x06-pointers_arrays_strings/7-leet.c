6acfc12ed1d95809bf6318f533e224ea4269e61f
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-03-28 13:48:55 +0100

    Created some new files

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,55 +10,18 @@ char *leet(char *r)
 {
 	int i, count = 0;
 	char *a = "4", *e = "3", *t = "7", *l = "1", *o = "0";
-	
+	char *f = "AETOLaetol";
+	char *w = "43701";
+
 	while (*(r + count) != '\0')
 		count++;
 	for (i = 0; i < count; i++)
 	{
-		if ((r[i]) == ('a'))
+		for (int d = 0; d < 10; d++)
 		{
-			*(r + i) = *a;
+			if (*(r + i) == *(f + d))
+			       *(r + i) = *(w + (d % 5));
 		}
-		else if (*(r + i) == ('A'))
-		{
-			*(r + i) = *a;
-		}
-		else if (*(r + i) == ('e'))
-		{
-			*(r + i) = *e;
-		}
-		else if (*(r + i) == ('E'))
-		{
-			*(r + i) = *e;
-		}
-		else if (*(r + i) == ('t'))
-		{
-			*(r + i) = *t;
-		}
-		else if (*(r + i) == ('T'))
-		{
-			*(r + i) = *t;
-		}
-		else if (*(r + i) == ('o'))
-		{
-			*(r + i) = *o;
-		}
-		else if (*(r + i) == ('O'))
-		{
-			*(r + i) = *o;
-		}
-		else if (*(r + i) == ('l'))
-		{
-			*(r + i) = *l;
-		}
-		else if (*(r + i) == ('L'))
-		{
-			*(r + i) = *l;
-		}
-		else
-		{
-			continue;
-		}
-	}
+	}		
 	return (r);
 }
