f1ef6c54cf75343793703e2d3f7060cefca1a138
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-03-31 00:03:53 +0100

    Once an idea has taken hold of the brain, Its almost Impossible to eradicate

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - A function that returns
+ *  the value of x raised to the power of y
+ *  @x: the number
+ *  @y: the power
+ *  Return: the squared number
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
