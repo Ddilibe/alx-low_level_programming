f09d22461f55387f26ce6098ef354450740054d3
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-03-31 01:06:25 +0100

    Worked on the 2-strlen_recursive.c file

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,25 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strlen_recursion - Function that return 
+ * _strlen_recursion - Function that return
  * the length of a string
  * @s: the string
  * Return: a number
  */
 
-int e = 0;
 
 int _strlen_recursion(char *s)
 {
 
 	if (*(s + 1) != '\0')
 	{
-		_strlen_recursion(s + 1);
-		e = e + 1;
+		return (1 + _strlen_recursion(s + 1));
 	}
 	else
 	{
-		e = e + 1;
+		return (1);
 	}
-	return (e);
 }
