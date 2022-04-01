27642ba9760a36a03c7969f993bfa954e8885e82
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-04-01 14:24:53 +0100

    Without libraries what have we? We have no past and future

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#The start of the command
-gcc -c *.c
-# The command above is used to create object files of all c codes contained in the directory
+gcc -Wall -pedantic -Werror -Wextra -c *.c
 ar -rc liball.a *.o
-# This command is used to create a archive of all the object codes in the pressent directory. Liball.a is the name given to the archive.
 nm liball.a
-# The command above os used to index the archived codes. Liball.a still remains the arhive name
