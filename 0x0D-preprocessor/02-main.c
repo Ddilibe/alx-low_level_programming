dc953630208915455f0de473afc71e7279d9b7d3
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-04-11 13:09:38 +0100

    File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Function to print the name 
+ * of the file it was complied from
+ *
+ * Return: Always 0.
+ */
+
+
+int main(void)
+{
+	printf("%s", __FILE__ );
+	return (0);
+}
