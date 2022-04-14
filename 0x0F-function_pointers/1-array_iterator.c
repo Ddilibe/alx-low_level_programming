f48b1e282784a1e1c6d5924a1897b4c5ead21b37
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-04-14 01:15:59 +0100

    Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - Function that executes a function 
+ * array_iterator - Function that executes a function
  * given as a parameter on each element of an array
  * @array: input array
  * @size: size of the array
@@ -13,9 +13,12 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	long unsigned int f = 0;
-	
-	for (f = 0; f < size; f++)
+
+	if (array && action)
 	{
-		action(array[f]);
+		for (f = 0; f < size; f++)
+		{
+			action(array[f]);
+		}
 	}
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,67 +1,71 @@
 #include "3-calc.h"
 
 /**
- * op_add - adds two integers
- * @a: first integer
- * @b: second integer
+ * op_add - adds two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * Return: an integer.
+ * Return: add.
  */
-
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - subtracts two integers
- * @a: first integer
- * @b: second integer
+ * op_sub - subctracts two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * Return: an integer
+ * Return: difference.
  */
-
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - Multiplies two integers
- * @a: first integer
- * @b: second integer
+ * op_mul - multiplies two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * Return: an integer
+ * Return: multiplication.
  */
-
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - divides an integer by another
- * @a: first integer
- * @b: second integer
+ * op_div - divides two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * Return: an integer
+ * Return: division.
  */
-
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
 /**
- * op_mod - find the remainder after
- * dividing an integer by another
- * @a: first integer
- * @b: second integer
+ * op_mod - calculates the module of two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * Return: an integer
+ * Return: remainder of the division.
  */
-
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
