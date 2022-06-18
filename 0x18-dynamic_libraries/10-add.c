ffd2b69286c945551a473147e9d1b8939daded94
Ddilibe <franklinfidelugwuowo@gmail.com>
2022-06-18 03:05:08 +0100

    [Feature]: Completed the python project

---
+++ 0x18-dynamic_libraries/10-add.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - Function to add two variables
+ * @a: variable A
+ * @b: variable B
+ *
+ * Return: (a+b)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - Function to subtract two variables
+ * @a: variable A
+ * @b: variable B
+ *
+ * Return: (a-b)
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - Function to multiply two variables
+ * @a: variable A
+ * @b: variable B
+ *
+ * Return: (a*b)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - Function to divide two variables
+ * @a: variable A
+ * @b: variable B
+ *
+ * Return: (a/b)
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - Function to remainder two variables
+ * @a: variable A
+ * @b: variable B
+ *
+ * Return: (a%b)
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
