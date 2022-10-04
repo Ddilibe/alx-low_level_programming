d3a0ddbad532fcd3ca25d4edc690eec33c8cd811
Dilibe Fidelugwuowo <franklinfidelugwuowo@gmail.com>
2022-10-04 06:53:46 +0100

    [Fix]: Fixed the linear file

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,12 +5,13 @@
 /**
  * linear_search - Function that searches for a value in an array of integers
  * using the linear search algorithms
- * array: Pointer to the first element of the array to search in
- * size: Number of elements in the array
- * value: Value to search for
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ * Return: the value if true else -1
  */
 
-int linear_search(int *array, size_t size, int value)
+int linear_search (int *array, size_t size, int value)
 {
 	size_t j = 0;
 
